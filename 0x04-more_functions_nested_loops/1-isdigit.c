d9d4ded78fc1f600a64712cd1da95ca29f502bd1
root <iamfelixwanyoh@gmail.com>
2023-02-23 00:14:17 -0800

    more functions and nested loops

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* _isdigit - function that check if it's a digit
+* @c: takes in a character/digit
+* Return: 1 if digit and 0 if anything else
+*/
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	return (1);
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/test.c
@@ -0,0 +1,7 @@
+#include "main.h"
+
+int main(void)
+{
+	_putchar(1.4 +'0');
+	return (0);
+}
